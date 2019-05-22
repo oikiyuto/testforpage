f34798c8a6ff4883efc5c3cc4eacb52a43501277
yuto <oikiyuto@gmail.com>
2019-05-22 16:55:06 +0900

    Initial Commit

---
+++ testforpage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testforpage
+//
+//  Created by yutos on 2019/05/22.
+//  Copyright © 2019 yutos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
